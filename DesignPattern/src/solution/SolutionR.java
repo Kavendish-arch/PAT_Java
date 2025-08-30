4b65cac2d1634ffb8ab17ee9b8fb7dca35e3edc6
chenyingtao <chen_yingtao@126.com>
2025-08-30 08:50:26 +0800

    refactor(项目结构): 重构项目结构并重命名文件
    - 删除了算法题/Main.java 文件- 将算法题/SolutionR.java 重命名为 DesignPattern/src/solution/SolutionR.java
    - 更新了 SolutionR.java 文件中的包名和导入语句
    -调整了 ListNode 类的定义和使用

--- DesignPattern/src/solution/SolutionR.java
+++ DesignPattern/src/solution/SolutionR.java
@@ -1,13 +1,13 @@
-import java.util.List;
+package solution;
 
 /**
  * Definition for singly-linked list.
- * public class ListNode {
+ * public class solution.ListNode {
  *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ *     solution.ListNode next;
+ *     solution.ListNode() {}
+ *     solution.ListNode(int val) { this.val = val; }
+ *     solution.ListNode(int val, solution.ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class ListNode{
@@ -26,14 +26,14 @@ class Solution {
         ListNode cur = new ListNode();
         cur.next = null;
 
-        
+
         while(head.next != null){
-            
+
             ListNode temp = head.next;
             head.next = cur.next;
             cur.next = head;
             head = temp;
-            
+
         }
         head.next = cur.next;
         return head;
@@ -41,7 +41,7 @@ class Solution {
 
     public static void main(String[] args) {
         ListNode head = new ListNode(1);
-        // ListNode head = new ListNode(1);
+        // solution.ListNode head = new solution.ListNode(1);
         head.next = new ListNode(2);
         head.next.next = new ListNode(3);
         head.next.next.next = new ListNode(4);
